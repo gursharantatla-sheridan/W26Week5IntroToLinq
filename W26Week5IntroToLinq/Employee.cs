9f6360a86b0e875e3e13f7901d36973179dbac05
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-02-02 14:55:09 -0500

    emp class

---
+++ W26Week5IntroToLinq/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace W26Week5IntroToLinq
+{
+    // primary constructor
+    public class Employee(string firstName, string lastName, double salary)
+    {
+        public string FirstName { get; set; } = firstName;
+        public string LastName { get; set; } = lastName;
+        public double Salary { get; set; } = salary;
+
+        public override string ToString()
+        {
+            return $"{FirstName,-10} {LastName,-10} {Salary,10:C}";
+        }
+    }
+}
