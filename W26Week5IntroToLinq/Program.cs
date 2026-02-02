631f15496cb01fc8d7deffa8f5036eb069ad729e
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-02-02 14:46:42 -0500

    linq eg

--- W26Week5IntroToLinq/Program.cs
+++ W26Week5IntroToLinq/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = { 3, 4, 6, 2, 1, 3, 4, 5, 6, 7, 6, 9, 8 };
+
+            // query syntax
+            var greaterThan4 = from n in array
+                               where n > 4
+                               orderby n
+                               select n;
+
+            foreach (var i in greaterThan4)
+                Console.Write(i + " ");
+            Console.WriteLine("\n\n");
+
+
+            // method syntax
+            var greaterThan5 = array.Where(n => n > 5).OrderByDescending(n => n);
+
+            foreach (var i in greaterThan5)
+                Console.Write(i + " ");
+            Console.WriteLine("\n\n");
+
+
+
+            List<string> colors = new List<string>();
+            colors.Add("bLuE");
+            colors.Add("rUbY");
+            colors.Add("GrEEnE");
+            colors.Add("ReD");
+
+            var startsWithR = from c in colors
+                              let uppercaseColors = c.ToUpper()
+                              where uppercaseColors.StartsWith("R")
+                              orderby uppercaseColors
+                              select uppercaseColors;
+
+            foreach (var i in startsWithR)
+                Console.WriteLine(i);
+            Console.WriteLine("\n\n");
+
+
+            colors.Add("rUsT");
+            colors.Add("YeLLow");
+
+            // deferred execution
+            foreach (var i in startsWithR)
+                Console.WriteLine(i);
+            Console.WriteLine("\n\n");
         }
     }
 }
